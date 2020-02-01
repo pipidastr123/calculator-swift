ebda79c51f61772474b0b78076e06a81232f1a4d
Andrei Zhuravlev <dwasdx@gmail.com>
2020-02-01 23:03:04 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Андрей Журавлев on 01.02.2020.
+//  Copyright © 2020 Андрей Журавлев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
